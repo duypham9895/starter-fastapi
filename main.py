74c95f0e48466970b52b15d73fb1954739b26313
Duy Pham <duy.pham@bizzi.vn>
2021-04-05 23:15:02 +0700

    Complete lesson "Query Parameters"

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": {"name": "duy"}}
+@app.get("/blog")
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    return {
+        "data": {
+            "title": "blog list",
+            "limit": limit,
+            "published": published,
+            "sort": sort,
+        }
+    }
 
 
-@app.get("/about")
-def about():
-    return {"data": {"name": "about page"}}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": {"id": id}}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int, limit: int = 10, sort: Optional[str] = None):
+    return {
+        "data": {
+            "id": id,
+            "comments": ["hii", "great!!", "555"],
+            "limit": limit,
+            "sort": sort,
+        }
+    }
